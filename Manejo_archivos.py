51366eae6ed1526cec9a0d0c3b45af3102122aba
Jose Lazo <josemanuelsanchezlazo@gmail.com>
2021-09-29 23:51:39 -0500

    Function read PDF has been added, begining

--- Manejo_archivos.py
+++ Manejo_archivos.py
@@ -1,4 +1,8 @@
+from os import error
 import re
+import PyPDF2
+
+
 
 def read_file(path):
     with open(path, 'r', encoding = 'utf-8') as f:
@@ -8,10 +12,23 @@ def read_file(path):
                 print(line.rstrip())#Erase \n
             
 
-
+def read_pdf():
+    try:
+        pdfFileObj = open('./files/2020.pdf', 'rb')
+        pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+        pageObj = pdfReader.getPage(0)
+        texto = pageObj.extractText()
+        return texto
+    except error as e:
+        print(e)
+    finally:
+        pdfFileObj.close()
 def run():
     file_path = './files/file_1.txt'
     read_file(file_path)
+
+    textFile = read_pdf()
+    print(textFile)
 
 
 if __name__ == "__main__":
