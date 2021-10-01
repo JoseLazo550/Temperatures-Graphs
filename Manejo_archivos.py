a194140701363a57624c4b4cb8579c20fb18ec21
Jose Lazo <josemanuelsanchezlazo@gmail.com>
2021-09-30 22:54:21 -0500

    File Modified to read only txt with sort data

--- Manejo_archivos.py
+++ Manejo_archivos.py
@@ -1,34 +1,61 @@
-from os import error
+import os
 import re
 import PyPDF2
 
 
 
-def read_file(path):
+def get_data(path):
     with open(path, 'r', encoding = 'utf-8') as f:
-        for line in f:
-            
-            if line.startswith('G'):#Find line that starts with 'G'
-                print(line.rstrip())#Erase \n
-            
+        f_list = list(enumerate(f))
+        
+        print(f_list)
+        for i, data in f_list:
+            data_state = data[:len(data)-1]
+            if data_state == 'Aguascalientes':
+                print('Aguascalientes esta en la linea: '+ str(int(i)+1))
+    print('Data has been gotten')      
 
-def read_pdf():
+def extract_pdf():#Not yet
     try:
         pdfFileObj = open('./files/2020.pdf', 'rb')
         pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
         pageObj = pdfReader.getPage(0)
         texto = pageObj.extractText()
+
         return texto
-    except error as e:
-        print(e)
+    except:
+        print("Error al abrir el archivo")
     finally:
         pdfFileObj.close()
-def run():
-    file_path = './files/file_1.txt'
-    read_file(file_path)
 
-    textFile = read_pdf()
-    print(textFile)
+def save_data(text):#Not yet
+    
+    path_file = './files/2020t.txt'
+    with open(path_file, 'w', encoding ='utf-8') as f:
+        f.write(text)
+
+
+
+    
+        
+
+    #with open(path_file, 'w', encoding = 'utf-8') as f:
+        
+            
+
+
+def run():
+    print('Bienvenido\n')
+    print(os.getcwd())
+    files = os.listdir('./files')
+    for file in files:
+        print(file)
+    
+
+    # name_f = input('Ingresa el nombre del archivo que deseas graficar: ')
+    file_path = './files/2020t.txt'#+name_f
+    get_data(file_path)
+    
 
 
 if __name__ == "__main__":
